37176378610f4fbcbc7996ff69fc261cce73255d
megha0809 <megha22@navgurukul.org>
2024-03-06 10:52:32 +0530

    initial commit

---
+++ src/pages/moviesPage/MoviesPage.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import styles from "./MoviesPage.module.css";
+import MoviesChild from "../../components/moviesChild/MoviesChild";
+const MoviesPage = ({ movie, setMovie }) => {
+  const [moviesData, setMoviesData] = useState([]);
+
+  const fetchMovieData = async (search) => {
+    try {
+      const response = await fetch(
+        `https://www.omdbapi.com/?s=${search}&apikey=d10f4781`
+        );
+      const data = await response.json();
+      setMoviesData(data.Search);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchMovieData(movie);
+  }, [movie]);
+
+  return (
+    <div className={styles.moviesParent}>
+      {!moviesData ? (
+        <h1>Oops...not Found !</h1>
+      ) : (
+        moviesData.map((data) => {
+          return <MoviesChild key={data.imdbID} {...data} />;
+        })
+      )}
+    </div>
+  );
+};
+
+export default MoviesPage;
